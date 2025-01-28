271f5f12802f41a48e5fe222ef9403b927f830e8
vanguard1105 <vanguard951105@gmail.com>
2025-01-28 01:02:01 +0100

    feat: I.N. Parser basic script

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,59 @@
-import Image from "next/image";
+'use client'; // Mark the file as a client component  
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useEffect, useState } from 'react';  
+import Tabs from './components/ui/Tabs'
+const { TabNav, TabList, TabContent } = Tabs
+const Home: React.FC = () => {  
+  const [settings, setSettings] = useState({  
+    buy_percentage: '',  
+    max_buy: '',  
+    min_buy: '',  
+    total_invest_sol: '',  
+    each_token_buy_times: '',  
+    trader_tx_max_limit: '',  
+    max_marketcap: '',  
+    min_marketcap: '',  
+    buy_slippage: '',  
+    auto_retry_times: '',  
+    sell_slippage: '',  
+    tip: '',  
+    buy_gas_fee: '',  
+    sell_gas_fee: '',  
+  });  
+  const [tabIndex, setTabIndex] = useState(0);
+  // State for current time if needed  
+  const [currentTime, setCurrentTime] = useState(Date.now());  
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {  
+    const { name, value } = e.target;  
+    setSettings((prevSettings) => ({  
+      ...prevSettings,  
+      [name]: value,  
+    }));  
+  };  
+
+  return (  
+      <div className='flex flex-col bg-slate-700 m-1 rounded-xl h-screen'>
+        <div className='flex flex-row bg-slate-600 rounded-lg w-[100% -8px] m-1'>
+          <div className={`rounded-md w-[50%] text-center my-1 ml-1 cursor-pointer text-sm p-1 ${tabIndex === 0 ? 'bg-slate-900 text-white' : 'bg-[#33415580] text-slate-300'}`} onClick={() => setTabIndex(0)}>
+            Trading Settings
+          </div>
+          <div className={`rounded-md w-[50%] text-center my-1 mr-1 cursor-pointer text-sm p-[2px] ${tabIndex === 1 ? 'bg-slate-900 text-white' : 'bg-[#33415580] text-slate-300'}`} onClick={() => setTabIndex(1)}>
+            Table Configuration
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+        {tabIndex === 0?
+          <div className='flex flex-row bg-slate-900 mt-4 w-[100% - 8px] mx-1'>
+            abc
+          </div>
+          :
+          <div>
+
+          </div>
+        }
+        
+      </div>
+  );  
+};  
+
+export default Home;
